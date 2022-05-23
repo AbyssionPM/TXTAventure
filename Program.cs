932617ec5ffac8a854fe180c55d57533700434b9
AbyssionPM <91093497+AbyssionPM@users.noreply.github.com>
2022-05-23 14:00:11 +0200

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using TDAventure.Properties;
+
+namespace TDAventure
+{
+    internal class Program
+    {
+        public static int lifepoint = 3;
+        public static String adventurer;
+        public static String[] inventory = new String[2];
+        public static bool goUp = false;
+
+        static void setName()
+        {
+            Console.WriteLine("Bienvenue jeune aventurier, quel est votre nom ?");
+            adventurer = Console.ReadLine();
+        }
+
+        public static void Main(string[] args)
+        {
+            Function.ShowTitle();
+            String direction, retry;
+            setName();
+            Function.start();
+            Start:
+            direction = Function.forest(); // Forêt (départ)
+           if (direction == "g") // Direction => Clairière 
+           {
+               direction = Function.glade(); // Clairière
+               if (direction == "c") // Direction => Chateau
+               {
+                   direction = Function.castle(); // Chateau 
+                   if (direction == "p") // Direction => Porte
+                   {
+                       Console.WriteLine("Vous tombez nez-à-nez avec un garde, celui-ci vous attaque ! ");
+                       Function.setLifePoint(3);
+                       Console.WriteLine("Vous etes à " + lifepoint + "PV");
+                       Console.WriteLine("Game Over !");
+                   }else
+                   {
+                       direction = Function.corridor();
+                       if (direction == "d")
+                       {
+                           Console.WriteLine("Félicitations vous avez trouvé la princesse ! ");
+                       }
+                       else
+                       {
+                           Console.WriteLine("Vous tombez nez à nez avec un garde, celui-ci vous attaque !");
+                           Function.setLifePoint(3);
+                           Console.WriteLine("Game Over !");
+                       }
+                   }
+               }
+               else
+               {
+                   Function.randEncounter();
+                   if(lifepoint > 0){
+                        direction = Function.castle();
+                        if ((direction == "p") && (inventory[0] == "épée"))
+                        {
+                           Console.WriteLine("Vous vous trouvez face à face avec un garde, vous l'attaquez !");
+                           Function.randFight();
+                            if (lifepoint > 0)
+                            {
+                                direction = Function.corridor();
+                                if (direction == "d")
+                                {
+                                    Console.WriteLine("Félicitations vous avez trouvé la princesse ! ");
+                                }
+                                else if ((direction == "g") && (inventory[0] == "épée"))
+                                {
+                                    Console.WriteLine("Vous tombez nez à nez avec un garde, celui-ci vous attaque !");
+                                    Function.randFight();
+                                    if (lifepoint > 0)
+                                    {
+                                        Console.WriteLine("Vous revenez sur vos pas et ouvrez l'autre porte.");
+                                        Console.WriteLine("Félicitations ! Vous avez trouvé la princesse !");
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            direction = Function.corridor();
+                            if (direction == "d")
+                            {
+                                Console.WriteLine("Félicitations vous avez trouvé la princesse ! ");
+                            }
+                            else if ((direction == "g") && (inventory[0] == "épée"))
+                            {
+                                Console.WriteLine("Vous tombez nez à nez avec un garde, celui-ci vous attaque !");
+                                Function.randFight();
+                                if (lifepoint > 0)
+                                {
+                                    Console.WriteLine("Vous revenez sur vos pas et ouvrez l'autre porte.");
+                                    Console.WriteLine("Félicitations ! Vous avez trouvé la princesse !");
+                                }
+                            }
+                        }
+                   }
+                   else
+                   {
+                       Console.ReadKey();
+                   }
+               }
+           }
+           else
+           {
+               Console.WriteLine("Vous chutez dans un ravin, vous perdez 1 points de vie ! ");
+               Function.setLifePoint(1);
+               Console.WriteLine("Souhaitez-vous essayer de remonter ? input : (o|n)"); 
+               direction = Console.ReadLine();
+               if (direction == "o")
+               {
+                   while (goUp == false)
+                   {
+                       goUp = false;
+                       Random r = new Random();
+                       int rand = r.Next(1, 13);
+                       if (rand <= 3)
+                       {
+                           
+                           Console.WriteLine("Vous ne réussissez pas à remonter le ravin...");
+                           Console.WriteLine("Vous perdez 1 PV !");
+                           Function.setLifePoint(1);
+                           Console.WriteLine("Voulez-vous retenter votre chance ? (o|n)");
+                           retry = Console.ReadLine();
+                           while (retry == "o" && lifepoint > 0)
+                           {
+                               rand = r.Next(1, 13);
+                               if (rand <= 3)
+                               {
+                                   Console.WriteLine("Vous ne réussissez pas à remonter le ravin...");
+                                   Console.WriteLine("Vous perdez 1 PV !, il vous reste "+lifepoint+" PV.");
+                                   Function.setLifePoint(1);
+                                   Console.WriteLine("Voulez-vous retenter votre chance ? (o|n)");
+                                   retry = Console.ReadLine();
+                                   
+                               }
+                           }
+                           Console.WriteLine("Vous etes à "+lifepoint+" PV : Game Over !");
+                           Console.ReadKey();
+                       }
+                       else
+                       {
+                           Console.WriteLine(
+                               "Vous réussisez à grimper et vous sortir de là ! Vous rebroussez chemin...");
+                           goUp = true;
+                           goto Start;
+                       }
+                   }
+               }
+
+           }
+
+        }
+    }
+}
---
+++ Properties/Function.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace TDAventure.Properties
+{
+    public static class Function
+    {
+        public static void ShowTitle()
+        {
+            Console.WriteLine(" ________  __    __  ________         ______                                   __                                   ");
+            Console.WriteLine("|        \\|  \\  |  \\|        \\       /      \\                                 |  \\                                  ");
+            Console.WriteLine(" \\$$$$$$$$| $$  | $$ \\$$$$$$$$      |  $$$$$$\\ __     __   ______   _______  _| $$_    __    __   ______    ______  ");
+            Console.WriteLine("   | $$    \\$$\\/  $$   | $$         | $$__| $$|  \\   /  \\ /      \\ |       \\|   $$ \\  |  \\  |  \\ /      \\  /      \\ ");
+            Console.WriteLine("   | $$     >$$  $$    | $$         | $$    $$ \\$$\\ /  $$|  $$$$$$\\| $$$$$$$\\\\$$$$$$  | $$  | $$|  $$$$$$\\|  $$$$$$\\");
+            Console.WriteLine("   | $$    /  $$$$\\    | $$         | $$$$$$$$  \\$$\\  $$ | $$    $$| $$  | $$ | $$ __ | $$  | $$| $$   \\$$| $$    $$");
+            Console.WriteLine("   | $$   |  $$ \\$$\\   | $$         | $$  | $$   \\$$ $$  | $$$$$$$$| $$  | $$ | $$|  \\| $$__/ $$| $$      | $$$$$$$$");
+            Console.WriteLine("   | $$   | $$  | $$   | $$         | $$  | $$    \\$$$    \\$$     \\| $$  | $$  \\$$  $$ \\$$    $$| $$       \\$$     \\");
+            Console.WriteLine("    \\$$    \\$$   \\$$    \\$$          \\$$   \\$$     \\$      \\$$$$$$$ \\$$   \\$$   \\$$$$   \\$$$$$$  \\$$        \\$$$$$$$");
+        }
+        public static void setLifePoint(int damage)
+        {
+            Program.lifepoint -= damage;
+        }
+
+        public static void start()
+        {
+            Console.WriteLine( Program.adventurer+ ". Vous vous trouvez dans une fôret à la recherche de votre bien aimée," +
+                              " cette dernière a été capturée par un royaume ennemi." +
+                              " Vous disposez de " + Program.lifepoint + " PV.");
+        }
+
+        public static String forest()
+        {
+            String direction;
+            Console.WriteLine("Devant vous se présente deux chemins, un allant à droite, un allant à gauche, lequel choisissez-vous ? Input : (g|d)");
+            direction = Console.ReadLine();
+            return direction;
+        }
+
+        public static String glade()
+        {
+            String direction;
+            Console.WriteLine("Vous vous trouvez à présent dans une clairière, un objet brillant attire votre attention." +
+                              " désirez-vous aller le voir ou continuer votre chemin ? Input (c|o)");
+            direction = Console.ReadLine();
+            return direction;
+        }
+
+        public static String castle()
+        {
+            String direction;
+            Console.WriteLine("Vous êtes fâce au chateau, devant vous se trouve une porte et une fenêtre, par quelle moyen voulez-vous entrer ? " +
+                              " input : (p|f)");
+            direction = Console.ReadLine();
+            return direction;
+        }
+
+        public static String corridor()
+        {
+            String direction;
+            Console.WriteLine("Vous vous trouvez maintenant dans un couloir, deux portes se présentent à vous, une à droite et une à gauche." +
+                              " Laquelle décidez-vous d'emprunter ? input : (g|d)");
+            direction = Console.ReadLine();
+            return direction;
+        }
+
+        public static void randEncounter()
+        {
+            Random r = new Random();
+            int rand = r.Next(1, 7);
+            if (rand <= 3)
+            {
+                Console.WriteLine("Vous tombez nez-à-nez avec un monstre !");
+                setLifePoint(3);
+                Console.WriteLine("Vous etes à  : " + Program.lifepoint + " PV !");
+                Console.WriteLine("Game Over !");
+            }
+            else
+            {
+                Console.WriteLine("Vous trouvez une épée ! Félicitations !");
+                Program.inventory[0] = "épée";
+                Console.WriteLine("Vous continuez votre chemin vers le chateau, fier de votre nouvelle acquisition !");
+            }
+        }
+
+        public static void randFight()
+        {
+            Random r = new Random();
+            int rand = r.Next(1, 7);
+            if (rand <= 3)
+            {
+                Console.WriteLine("Vous gagnez le combat !");
+            }
+            else
+            {
+                Console.WriteLine("Le garde est plus entrainé que vous, il vous inflige 3 points de dégats !");
+                setLifePoint(3);
+                Console.WriteLine("Game Over !");
+            }
+        }
+    }
+}
